b9aee77846b6367d3c88f6dcc9508fef21d45144
Jen Sung <jensunngggg@gmail.com>
2019-02-19 13:08:33 -0500

    checkpoint 3 routing: completed

--- src/App.js
+++ src/App.js
@@ -3,29 +3,23 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-<<<<<<< HEAD
          <nav>
           <Link to='/'>Landing</Link>
           <Link to='/library'>Library</Link>
          </nav>
          <h1>BlocJams</h1>
-=======
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-        <h1> Bloc Jams</h1>
->>>>>>> checkpoint-3-routing
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+          <Route path="/album" component={Album} />
         </main>
       </div>
     );
